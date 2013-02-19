2f7e8c592cfd5e2f25b1c0490611968a9476c741
Kitamura Satoshi <with.no.parachute@gmail.com>
2013-02-19 16:27:35 +0900

    strip white spaces before doctype and after html close tag

--- plugins/spaceless_block.rb
+++ plugins/spaceless_block.rb
@@ -2,7 +2,7 @@ class Spaceless < Liquid::Block
   def render(context)
     output = super
 
-    %|#{output.gsub(/\>\s+\</, '><')}|
+    %|#{output.gsub(/>\s+</, '><').gsub(/\s+<\!/,'<!').gsub(/<\/html>\s+/, '</html>')}|
   end
 end
 
